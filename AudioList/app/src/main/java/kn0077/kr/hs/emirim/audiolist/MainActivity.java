52504afd52e529d3a53fe455164ef3ecdc28a23f
Kim kyuna <kkn00077@gmail.com>
2017-06-19 20:34:03 +0900

    #2 reference widget

--- AudioList/app/src/main/java/kn0077/kr/hs/emirim/audiolist/MainActivity.java
+++ AudioList/app/src/main/java/kn0077/kr/hs/emirim/audiolist/MainActivity.java
@@ -2,12 +2,34 @@ package kn0077.kr.hs.emirim.audiolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView list;
+    Button butPlay, butStop;
+    TextView textMusic;
+    ProgressBar progress;
+    String[] musics={"dean_admusic","dean_limbo_second","dean_outro"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list=(ListView)findViewById(R.id.list_music);
+        butPlay=(Button)findViewById(R.id.but_play);
+        butStop=(Button)findViewById(R.id.but_stop);
+        textMusic=(TextView)findViewById(R.id.text_music);
+        progress=(ProgressBar)findViewById(R.id.progress_music);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,musics);
+        list.setAdapter(adapter);
+        list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        list.setItemChecked(0,true);
     }
 }
