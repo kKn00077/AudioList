6cecb3c673679515f00ec57ff067213a45e08896
Kim kyuna <kkn00077@gmail.com>
2017-06-19 20:00:20 +0900

    #1 make layout

---
+++ IndividualApp/app/src/main/java/kr/hs/emirim/dahoe0304/individualapp/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.dahoe0304.individualapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
